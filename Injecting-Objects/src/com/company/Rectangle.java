e81804062b7586d7133e74e3f209f7b1e1f8178c
hansmbua <hansmbua@yahoo.com>
2022-02-08 18:23:34 +0100

    injection-object added

---
+++ Injecting-Objects/src/com/company/Rectangle.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Rectangle {
+
+
+
+    public void DrawNow(){
+        System.out.println(" Rectangle drawn");
+    }
+
+
+}
---
+++ Injecting-Objects/src/com/company/Triangle.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Triangle {
+    private String type;
+    private int hight;
+
+    public Triangle( String type){
+        this.type = type;
+
+    }
+    public Triangle( String type, int hight){
+        this.type = type;
+        this.hight = hight;
+
+    }
+
+    public Triangle( int hight){
+        this.hight = hight;
+
+    }
+
+    public void Draw(){
+        System.out.println( getType()+" triangle drawn with hight of "+getHight());
+
+
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+
+    public int getHight() {
+        return hight;
+    }
+}
