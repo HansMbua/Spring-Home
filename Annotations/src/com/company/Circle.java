eacedff185e627553e4c5e78fade4ea1bbb21c6a
hansmbua <hansmbua@yahoo.com>
2022-02-15 02:50:07 +0100

    added new projects

---
+++ Annotations/src/com/company/Circle.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import org.springframework.beans.factory.annotation.Required;
+
+public class Circle implements Shaps{
+    private Point center;
+
+
+
+    @Override
+    public void Draw() {
+        System.out.println("Circle Point is : ( "+center.getX()+" ,"+ center.getY()+" )");
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    @Required
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+}
---
+++ Coding To Interfaces/src/com/company/Circle.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Circle implements Shaps{
+    private Point center;
+
+
+
+    @Override
+    public void Draw() {
+        System.out.println("Circle Point is : ( "+center.getX()+" ,"+ center.getY()+" )");
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+}
