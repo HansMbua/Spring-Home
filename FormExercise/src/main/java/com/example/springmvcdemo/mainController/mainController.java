caa87623690b0b79c8e5e7a7aea08d854631a90f
hansmbua <hansmbua@yahoo.com>
2022-06-29 16:25:15 +0200

    solved

---
+++ FormExercise/src/main/java/com/example/springmvcdemo/mainController/mainController.java
@@ -0,0 +1,12 @@
+package com.example.springmvcdemo.mainController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class mainController {
+    @RequestMapping("/")
+    public String homePage(){
+        return "Home";
+    }
+}
---
+++ FormExercise/src/main/java/com/example/springmvcdemo/studentController.java
@@ -0,0 +1,28 @@
+package com.example.springmvcdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class studentController {
+
+    @RequestMapping("/showForm")
+    public String studentForm(Model model){
+        //creat student object
+        Student theStudent = new Student();
+
+        //create a model
+        model.addAttribute("student",theStudent);
+        return "studentForm";
+    }
+    @RequestMapping("/processForm")
+    public String processForm(){
+
+        return "confirmationPage";
+    }
+
+
+
+}
